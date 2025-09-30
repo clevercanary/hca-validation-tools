5362b1a925bd170afd11b6b011f056395d130503
hunterckx <118154470+hunterckx@users.noreply.github.com>
2025-09-29 21:01:53 -0700

    feat: add poetry project to call cellxgene validator (#140)

---
+++ services/cellxgene-validator/src/cellxgene_validator/main.py
@@ -0,0 +1,44 @@
+import sys
+import logging
+import json
+from cellxgene_schema.validate import validate
+
+class ListHandler(logging.Handler):
+    """
+    Custom logging handler to capture warning and error messages.
+    """
+    def __init__(self, warning_list, error_list):
+        super().__init__()
+        self.warning_list = warning_list
+        self.error_list = error_list
+
+    def emit(self, record):
+        if record.levelno == logging.ERROR:
+           self.error_list.append(self.format(record))
+        elif record.levelno == logging.WARNING:
+           self.warning_list.append(self.format(record))
+
+def run_validator():
+  if len(sys.argv) < 2:
+    raise Exception("Missing command line argument for file path")
+  
+  file_path = sys.argv[1]
+
+  logger = logging.getLogger("cellxgene_schema.validate")
+  logger.setLevel(logging.WARNING)
+  warning_messages = []
+  error_messages = []
+  logger.addHandler(ListHandler(warning_messages, error_messages))
+
+  is_valid, _, _ = validate(file_path)
+
+  result = {
+     "valid": is_valid,
+     "warnings": warning_messages,
+     "errors": error_messages
+  }
+
+  print(json.dumps(result))
+
+if __name__ == "__main__":
+  run_validator()
