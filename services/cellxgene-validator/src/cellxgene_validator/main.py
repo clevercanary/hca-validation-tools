022b349388766011071cb92e1473427e5583f6aa
hunterckx <118154470+hunterckx@users.noreply.github.com>
2025-10-02 00:05:55 -0700

    style: make indentation consistent in cellxgene validator script (#140)

--- services/cellxgene-validator/src/cellxgene_validator/main.py
+++ services/cellxgene-validator/src/cellxgene_validator/main.py
@@ -4,19 +4,19 @@ import json
 from cellxgene_schema.validate import validate
 
 class ListHandler(logging.Handler):
-    """
-    Custom logging handler to capture warning and error messages.
-    """
-    def __init__(self, warning_list, error_list):
-        super().__init__()
-        self.warning_list = warning_list
-        self.error_list = error_list
+  """
+  Custom logging handler to capture warning and error messages.
+  """
+  def __init__(self, warning_list, error_list):
+    super().__init__()
+    self.warning_list = warning_list
+    self.error_list = error_list
 
-    def emit(self, record):
-        if record.levelno == logging.ERROR:
-           self.error_list.append(self.format(record))
-        elif record.levelno == logging.WARNING:
-           self.warning_list.append(self.format(record))
+  def emit(self, record):
+    if record.levelno == logging.ERROR:
+      self.error_list.append(self.format(record))
+    elif record.levelno == logging.WARNING:
+      self.warning_list.append(self.format(record))
 
 def run_validator(file_path):
   logger = logging.getLogger("cellxgene_schema.validate")
@@ -28,14 +28,14 @@ def run_validator(file_path):
   try:
     is_valid, _, _ = validate(file_path)
   except Exception as e:
-     is_valid = False
-     warning_messages = []
-     error_messages = [f"Encountered an unexpected error while calling CELLxGENE validator: {e}"]
+    is_valid = False
+    warning_messages = []
+    error_messages = [f"Encountered an unexpected error while calling CELLxGENE validator: {e}"]
 
   return {
-     "valid": is_valid,
-     "warnings": warning_messages,
-     "errors": error_messages
+    "valid": is_valid,
+    "warnings": warning_messages,
+    "errors": error_messages
   }
 
 if __name__ == "__main__":
