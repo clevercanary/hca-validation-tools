3dd9f6b7639f80e2968351cc4cc4b9e541c4d1ec
David Rogers <dave@clevercanary.com>
2025-10-24 22:35:42 -0700

    feat: added hca-schema-validator package (#152) (#153)

---
+++ packages/hca-schema-validator/src/hca_schema_validator/_vendored/__init__.py
@@ -0,0 +1,18 @@
+"""
+Vendored dependencies for hca-schema-validator.
+
+This directory contains vendored copies of third-party code that has been
+modified for HCA-specific use.
+
+Vendored packages:
+- cellxgene_schema: Modified version of cellxgene-schema
+  Original: https://github.com/chanzuckerberg/single-cell-curation
+  Copyright (c) 2020-Present Chan Zuckerberg Initiative
+  License: MIT (see cellxgene_schema/LICENSE)
+  
+  This code has been vendored and modified to support HCA-specific validation
+  requirements. All modifications are also licensed under MIT.
+
+The vendored code is maintained as part of this package and does not require
+external updates.
+"""
